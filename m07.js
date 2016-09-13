51ba42498010a68a073a8128918957f6c8f519c6
IsaV <isaverkes@hotmail.com>
2016-09-13 02:55:10 +0000

    add files for HW

---
+++ m07.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var request = require('request');
+request('http://visualizedata.github.io/datastructures/data/m07.html', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body) // Show the HTML for the Google homepage. 
+    }else {
+        console.log(error)
+    }
+})
+
+http.createServer(function (req, res) {
+  
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World from Cloud9\n');
+}).listen(process.env.PORT);
